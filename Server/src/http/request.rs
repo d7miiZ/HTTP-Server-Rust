8c52cc6bb8bf24dfc28270d6709c844da3f355ac
AbdulrahmanSaleh <439101980@student.ksu.edu.sa>
2021-07-27 17:29:11 +0300

    Adding life time to request

--- Server/src/http/request.rs
+++ Server/src/http/request.rs
@@ -4,41 +4,54 @@ use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtRes};
 use std::str;
 
-pub struct Request {
+pub struct Request<'buf_life> {
     function: Function,
-    path: String,
-    query: Option<String>,
+    path: &'buf_life str,
+    query: Option<&'buf_life str>,
 }
 
-impl Request {
-    fn convert_to_request(buffer: &[u8]) -> Result<Self, String> {
-        unimplemented!();
-    }
-}
+// impl <'buf_life> Request<'buf_life> {
+//     fn convert_to_request(buffer: &'buf_life [u8]) -> Result<Self, String> {
+//         unimplemented!();
+//     }
+// }
 
-impl TryFrom<&[u8]> for Request {
+impl <'buf_life> TryFrom<&'buf_life [u8]> for Request<'buf_life> {
     type Error = RequestError;
 
-    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buffer: &'buf_life [u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buffer).or(Err(RequestError::InvalidEncoding))?;
 
-        let (function , rest_of_request) = explode(request, ' ').ok_or(RequestError::InvalidRequest)?;
-        let (path_and_query , rest_of_request) = explode(rest_of_request, ' ').ok_or(RequestError::InvalidRequest)?;
-        let (protocol , _) = explode(rest_of_request, ' ').ok_or(RequestError::InvalidRequest)?;
+        let (function, rest_of_request) =
+            explode(request, ' ').ok_or(RequestError::InvalidRequest)?;
+        let (path_and_query, rest_of_request) =
+            explode(rest_of_request, ' ').ok_or(RequestError::InvalidRequest)?;
+        let (protocol, _) = explode(rest_of_request, ' ').ok_or(RequestError::InvalidRequest)?;
 
-        if protocol != "HTTP/1.1"{
+        if protocol != "HTTP/1.1" {
             return Err(RequestError::InvalidProtocol);
         }
 
         let function: Function = function.parse()?;
+        let mut path = path_and_query;
+        let mut query = None;
 
-        unimplemented!()
+        if let Some((found_path, found_query)) = explode(path_and_query, '?') {
+            path = found_path;
+            query = Some(found_query);
+        }
+
+        Ok(Self {
+            function,
+            path,
+            query,
+        })
     }
 }
 
 fn explode(text: &str, sep: char) -> Option<(&str, &str)> {
     for (index, letter) in text.chars().enumerate() {
-        if letter == sep || letter == '\r'{
+        if letter == sep || letter == '\r' {
             return Some((&text[..index - 1], &text[index + 1..]));
         }
     }
---
+++ Server/target/debug/deps/server.d
@@ -0,0 +1,9 @@
+C:\Users\Abdul\Documents\GitHub\HTTP-Server-Rust\Server\target\debug\deps\server.exe: src\main.rs src\server.rs src\http\mod.rs src\http\request.rs src\http\function.rs
+
+C:\Users\Abdul\Documents\GitHub\HTTP-Server-Rust\Server\target\debug\deps\server.d: src\main.rs src\server.rs src\http\mod.rs src\http\request.rs src\http\function.rs
+
+src\main.rs:
+src\server.rs:
+src\http\mod.rs:
+src\http\request.rs:
+src\http\function.rs:
